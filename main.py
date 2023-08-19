ba5220fbfa676c2b1c1ab9a29dd3e806e253c9eb
JosiasAurel <ndjosiasaurel@gmail.com>
2023-08-19 16:49:32 +0100

    mix stuff

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from sqlmodel import select, Session
 from database import create_db_and_tables, engine
 from models import User, Shop, Product
-
-from fastapi import FastAPI
+from typing import Annotated
+import secrets
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
@@ -25,7 +27,7 @@ async def read_users():
         return Users
     
 @app.post("/shops/")
-async def  create_shop(shop:Shop):
+async def create_shop(shop:Shop):
     with Session(engine) as session:
         session.add(shop)
         session.commit()
@@ -39,7 +41,7 @@ async def read_shops():
         return Shops
     
 @app.post("/products/")
-async def  create_shop(product:Product):
+async def  create_product(product:Product):
     with Session(engine) as session:
         session.add(product)
         session.commit()
@@ -47,7 +49,26 @@ async def  create_shop(product:Product):
         return product
     
 @app.get("/Products/")
-async def read_shops():
+async def read_products():
     with Session(engine) as session:
         Products = session.exec(select(Product)).all()
         return
+
+@app.post("/upload")
+async def upload_file(
+    file: Annotated[UploadFile, File()],
+    token: Annotated[str, Form()]
+):
+    try:
+        if file.content_type:
+            extension = file.content_type.split("/")
+            content = await file.read()
+            disk_filename = f"{secrets.token_urlsafe(10)}.{extension[-1]}"
+
+            # write the file to disk
+            with open(disk_filename, "wb") as image_file:
+                image_file.write(content)
+            return { "msg": f"wrote file to {disk_filename}" }
+    except:
+        return { "msg": "Failed" }
+
